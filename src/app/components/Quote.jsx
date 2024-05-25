681ce7c6df6c2a62ecd05f0702263a14d3c99c63
Olakareem <olakareemomobolarinwa@gmail.com>
2024-05-25 18:56:32 +0100

    second commit

--- src/app/components/Quote.jsx
+++ src/app/components/Quote.jsx
@@ -4,16 +4,30 @@ import html2pdf from 'html2pdf.js';
 const Quote = () => {
   const quoteRef = useRef();
 
-  const handleDownload = () => {
-    const element = quoteRef.current;
-    const opt = {
-      margin: 1,
-      filename: 'quote.pdf',
-      image: { type: 'jpeg', quality: 0.98 },
-      html2canvas: { scale: 2 },
-      jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' },
-    };
-    html2pdf().from(element).set(opt).save();
+//   const handleDownload = () => {
+//     const element = quoteRef.current;
+//     const opt = {
+//       margin: 1,
+//       filename: 'quote.pdf',
+//       image: { type: 'jpeg', quality: 0.98 },
+//       html2canvas: { scale: 2 },
+//       jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' },
+//     };
+//     html2pdf().from(element).set(opt).save();
+//   };
+
+const handleDownload = () => {
+    if (isClient) {
+      const element = quoteRef.current;
+      const opt = {
+        margin: 1,
+        filename: 'quote.pdf',
+        image: { type: 'jpeg', quality: 0.98 },
+        html2canvas: { scale: 2 },
+        jsPDF: { unit: 'in', format: 'letter', orientation: 'portrait' },
+      };
+      html2pdf().from(element).set(opt).save();
+    }
   };
 
   return (
