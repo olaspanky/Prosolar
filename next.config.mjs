888491bd4eb8399ba35085b2ebd360c216af46a8
Olakareem <olakareemomobolarinwa@gmail.com>
2024-12-04 20:00:27 +0100

    updates

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    missingSuspenseWithCSRBailout: false
+    experimental: {
+      missingSuspenseWithCSRBailout: false,
+    },
   };
-  
-  export default nextConfig;
