317ffb70abdc41fbf58eb091ef27b7a7e7679bd2
Olakareem <olakareemomobolarinwa@gmail.com>
2024-12-04 19:47:10 +0100

    updates

--- src/app/components/Provider.js
+++ src/app/components/Provider.js
@@ -1,13 +1,30 @@
-"use client"
-import { ReactNode } from "react"
-import { CartProvider } from "react-use-cart"
+'use client';
 
-export default function CartProviders ({children}) {
-    return(
-        <CartProvider >
-        {children}
+import { Suspense, useEffect } from 'react';
+import { usePathname, useSearchParams } from 'next/navigation';
+
+export function Providers({ children }) {
+  const pathname = usePathname();
+  const searchParams = useSearchParams();
+
+  useEffect(() => {
+    const initPixel = async () => {
+      try {
+        const ReactPixel = (await import('react-facebook-pixel')).default;
         
-        </CartProvider>
+        ReactPixel.init('382348839135035'); // Replace with your Facebook Pixel ID
+        ReactPixel.pageView();
+      } catch (error) {
+        console.error('Failed to initialize Facebook Pixel', error);
+      }
+    };
 
-    )
+    initPixel();
+  }, [pathname, searchParams]);
+
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      {children}
+    </Suspense>
+  );
 }
