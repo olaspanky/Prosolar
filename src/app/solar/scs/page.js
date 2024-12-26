d1a0ada4cf0ed6f890b9625c4272ab777adb69b2
Olakareem <olakareemomobolarinwa@gmail.com>
2024-12-26 04:02:39 +0100

    fix

--- src/app/solar/scs/page.js
+++ src/app/solar/scs/page.js
@@ -3,6 +3,9 @@ import Image from "next/image";
 import Nav from "@/app/components/Nav"
 import Head from "../../components/pack2/Head"
 import { Hero } from "../../components/pack2/Hero";
+
+export const dynamic = "force-dynamic"
+
 export default function Home() {
   return (
     <main className="bg-white ">
--- src/app/solar/shs/page.js
+++ src/app/solar/shs/page.js
@@ -3,6 +3,9 @@ import Image from "next/image";
 import Nav from "@/app/components/Nav"
 import Head from "../../components/pack/Head"
 import { Hero } from "../../components/pack/Hero";
+
+export const dynamic = "force-dynamic"
+
 export default function Home() {
   return (
     <main className="bg-white ">
