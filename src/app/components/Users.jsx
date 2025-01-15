9e7513b42b442d2933c6b9d9f376d7d3d491e7d6
Olakareem <olakareemomobolarinwa@gmail.com>
2025-01-15 02:08:53 +0100

    fixes

--- src/app/components/Users.jsx
+++ src/app/components/Users.jsx
@@ -175,7 +175,7 @@ export default function Users() {
 
   return (
     <div className="p-6 bg-gray-100 ">
-      <h1 className="text-2xl font-bold mb-6 text-gray-800">User Submissions</h1>
+      <h1 className="text-2xl font-bold mb-6 text-gray-800">Auto Quote Submissions</h1>
       <div className="mb-4">
         <button
           onClick={handleDownloadCSV}
