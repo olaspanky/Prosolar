b19b39d02c0d369ae2807bb593fccd787002fc07
Olakareem <olakareemomobolarinwa@gmail.com>
2025-02-17 21:16:19 +0100

    fix

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,4 +1,3 @@
-
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Syne, Plus_Jakarta_Sans } from 'next/font/google';
@@ -7,10 +6,8 @@ import "slick-carousel/slick/slick-theme.css";
 import { Footer } from "./components/Footer";
 import GoogleAnalytics from "./components/GoogleAnalytics";
 import { Providers } from "./Provider";
-import { Suspense } from 'react'
+import { Suspense } from 'react';
 
-
-// import Pixel from "./components/Pixel";
 const syne = Syne({
   weight: '700',
   subsets: ['latin'],
@@ -35,63 +32,86 @@ export const metadata = {
 };
 
 export default function RootLayout({ children }) {
-
-  
   return (
     <html lang="en" className={`${syne.variable} ${syne2.variable} ${jak.variable}`}>
+      <head>
+        {/* Google Tag Manager */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+              new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+              j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+              'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+              })(window,document,'script','dataLayer','GTM-5593KCRS');
+            `,
+          }}
+        />
+        {/* End Google Tag Manager */}
+      </head>
       <body>
-      <Suspense>
-      <Providers/>
+        <Suspense>
+          <Providers />
         </Suspense>
-          <GoogleAnalytics />
-          
-          <div className="w-[100vw] min-h-screen">
-            {children}
-          </div>
-          
-          {/* LinkedIn Insight Tag */}
-          <script
-            type="text/javascript"
-            dangerouslySetInnerHTML={{
-              __html: `
-                _linkedin_partner_id = "1613905";
-                window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
-                window._linkedin_data_partner_ids.push(_linkedin_partner_id);
-              `,
-            }}
+        <GoogleAnalytics />
+        <div className="w-[100vw] min-h-screen">
+          {children}
+        </div>
+
+        {/* LinkedIn Insight Tag */}
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              _linkedin_partner_id = "1613905";
+              window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+              window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+            `,
+          }}
+        />
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+              (function(l) {
+                if (!l) {
+                  window.lintrk = function(a,b) {
+                    window.lintrk.q.push([a,b]);
+                  };
+                  window.lintrk.q = [];
+                }
+                var s = document.getElementsByTagName("script")[0];
+                var b = document.createElement("script");
+                b.type = "text/javascript";
+                b.async = true;
+                b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
+                s.parentNode.insertBefore(b, s);
+              })(window.lintrk);
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            alt=""
+            src="https://px.ads.linkedin.com/collect/?pid=1613905&fmt=gif"
           />
-          <script
-            type="text/javascript"
-            dangerouslySetInnerHTML={{
-              __html: `
-                (function(l) {
-                  if (!l) {
-                    window.lintrk = function(a,b) {
-                      window.lintrk.q.push([a,b]);
-                    };
-                    window.lintrk.q = [];
-                  }
-                  var s = document.getElementsByTagName("script")[0];
-                  var b = document.createElement("script");
-                  b.type = "text/javascript";
-                  b.async = true;
-                  b.src = "https://snap.licdn.com/li.lms-analytics/insight.min.js";
-                  s.parentNode.insertBefore(b, s);
-                })(window.lintrk);
-              `,
-            }}
-          />
-          <noscript>
-            <img
-              height="1"
-              width="1"
-              style={{ display: 'none' }}
-              alt=""
-              src="https://px.ads.linkedin.com/collect/?pid=1613905&fmt=gif"
-            />
-          </noscript>
-          {/* <Pixel name='FACEBOOK_PIXEL_1' /> */}
-          <Footer />
+        </noscript>
+
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5593KCRS"
+            height="0"
+            width="0"
+            style={{ display: 'none', visibility: 'hidden' }}
+          ></iframe>
+        </noscript>
+        {/* End Google Tag Manager (noscript) */}
+
+        <Footer />
       </body>
     </html>
   );
